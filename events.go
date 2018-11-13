8a41bd8bc8eb3ac0ba3615dbe1f6614653de2d75
xingcuntian <xingcuntian@qq.com>
2018-11-13 17:17:40 +0800

    update

--- events.go
+++ events.go
@@ -4,29 +4,29 @@ package vuego
 
 import "syscall/js"
 
-type XMLHttpRequest struct{
-  js.Value
+type XMLHttpRequest struct {
+	js.Value
 }
 
 func NewEvent() *XMLHttpRequest {
-  return &XMLHttpRequest{
-    Value: js.Global().Get("XMLHttpRequest").New()
-  }
+	return &XMLHttpRequest{
+		Value: js.Global().Get("XMLHttpRequest").New(),
+	}
 }
 
-func (x *XMLHttpRequest) Open(method string, path string){
-  x.Value.Call("open",method,path)
+func (x *XMLHttpRequest) Open(method string, path string) {
+	x.Value.Call("open", method, path)
 }
 
-func (x *XMLHttpRequest) OnLoad(fn func([]js.Value)){
-  cb := js.NewCallback(fn)
-  x.Value.Set("onload",cb)
+func (x *XMLHttpRequest) OnLoad(fn func([]js.Value)) {
+	cb := js.NewCallback(fn)
+	x.Value.Set("onload", cb)
 }
 
 func (x *XMLHttpRequest) Send() {
-  x.Value.Call("send")
+	x.Value.Call("send")
 }
 
 func (x *XMLHttpRequest) GetResponseText() string {
-  return x.Get("responseText").String()
+	return x.Get("responseText").String()
 }
